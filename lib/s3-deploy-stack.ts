8bbbfe6ef24f1289415e6a5c1118a5a2caf89abe
S Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2024-02-19 07:04:01 +0530

    add removal policy

--- lib/s3-deploy-stack.ts
+++ lib/s3-deploy-stack.ts
@@ -7,11 +7,14 @@ export class BucketDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const target_bucket = new s3.Bucket(this, 'MyBucket', {});
+    const target_bucket = new s3.Bucket(this, 'MyBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
     new s3deploy.BucketDeployment(this, 'DeployAssets', {
       sources: [s3deploy.Source.asset('./src')],
       destinationBucket: target_bucket,
     });
+
   }
 }
